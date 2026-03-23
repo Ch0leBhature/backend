b67b5c85867dce54a535f5c9947bced43847e493
Ch0leBhature <225065954+Ch0leBhature@users.noreply.github.com>
2026-03-23 14:28:39 +0530

    db connected

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import app from "express";
+
+const app = express();
+
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
+import dotenv from "dotenv"
 
+import express from "express";
+import connectDB from "./db/index.js";
+const app = express()
+
+dotenv.config({
+  path:'./env'
+})
+
+connectDB()
+.then(()=>{
+      const server = app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running at port: ${process.env.PORT}`);
+      })
+      
+      server.on("error",(error)=>{
+        console.log("SERVER ERR: ",error);
+        process.exit(1)
+      })
+
+
+  })
+.catch((err)=>{
+    console.log("MONGO DB CONNECTION ERROR!!!",err);
+  })
+
+//can be done like this but better to keep it in db folder
+// //iifi
+// ;(async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",()=>{
+//             console.log("ERR: ",error);
+//             throw error
+//     })
+//
+//     app.listen(process.env.PORT,()=>{
+//         console.log(`App is listening on ${process.env.PORT}`);
+//     })
+//
+//     }catch(error){
+//         console.log("ERROR: ",error);
+//         throw err
+//     }
+// })()
